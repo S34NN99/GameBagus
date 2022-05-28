5253c1946df1ed7062c9efc53d9b1701b01691a1
theBlitzzz <65937529+theBlitzz@users.noreply.github.com>
2022-05-28 16:51:29 +0800

    Sync

--- GameBagus Prototype/Assets/Actions/CrunchAction.cs
+++ GameBagus Prototype/Assets/Actions/CrunchAction.cs
@@ -7,5 +7,7 @@ public class CrunchAction : BaseAction {
 
     public override void ActOn(Candle candle) {
         print(candle.name + " is crunching");
+        candle.SM.workingState.Exit(candle);
+        candle.SM.SetWorkingState(new W_Crunch());
     }
 }
