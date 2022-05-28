906101f43bf711353eada5899c6ea4df69e459c8
theBlitzzz <65937529+theBlitzz@users.noreply.github.com>
2022-05-29 00:20:25 +0800

    Super Wonky head roll

--- GameBagus Prototype/Assets/Candles/Candle.cs
+++ GameBagus Prototype/Assets/Candles/Candle.cs
@@ -85,6 +85,7 @@ public class Candle : MonoBehaviour, IEntity {
     }
 
     public void Death() {
+        HeadManager.Instance.RollHead(HeadImage.gameObject);
         Destroy(gameObject);
     }
 
