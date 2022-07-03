600e510110cdd62a3d74c457848e64e14f744fee
theBlitzzz <65937529+theBlitzz@users.noreply.github.com>
2022-07-03 18:40:50 +0800

    Refactored the Candle script
    Removed dependencies to Candle Manager and Head Manager

--- GameBagus Prototype/Assets/Scripts/HeadManager.cs
+++ GameBagus Prototype/Assets/Scripts/HeadManager.cs
@@ -4,16 +4,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class HeadManager : MonoBehaviour {
-    private static HeadManager _instance;
-    public static HeadManager Instance {
-        get {
-            if (_instance == null) {
-                _instance = new GameObject("Head Manager").AddComponent<HeadManager>();
-            }
-            return _instance;
-        }
-    }
-
     [SerializeField] private float posThreshold = 5f;
     [SerializeField] private float posSmoothSpeed = 10f;
     [SerializeField] private float dropSpeed = 15f;
@@ -23,48 +13,38 @@ public class HeadManager : MonoBehaviour {
     private int headCount;
     public int HeadCount => headCount;
 
-    private void Awake() {
-        if (_instance == null) {
-            _instance = this;
-        } else if (_instance != this) {
-            Destroy(this);
-        }
-
-    }
-
     public void RollHead(GameObject head) {
-        head.transform.SetParent(transform);
+        RectTransform clonedHead = Instantiate(head, transform).GetComponent<RectTransform>();
 
         headCount++;
         StartCoroutine(RollHeadCoroutine());
 
         IEnumerator RollHeadCoroutine() {
-            RectTransform headTransform = head.GetComponent<RectTransform>();
-            headTransform.anchorMin = new Vector2(0.5f, 0.5f);
-            headTransform.anchorMax = new Vector2(0.5f, 0.5f);
-            headTransform.pivot = new Vector2(0.5f, 0.5f);
+            clonedHead.anchorMin = new Vector2(0.5f, 0.5f);
+            clonedHead.anchorMax = new Vector2(0.5f, 0.5f);
+            clonedHead.pivot = new Vector2(0.5f, 0.5f);
 
             Vector2 posSmoothVelocity = new Vector2(0, 0);
 
-            while ((headTransform.anchoredPosition - dropAnchor.anchoredPosition).magnitude > posThreshold) {
-                Vector2 currentPos = Vector2.SmoothDamp(headTransform.anchoredPosition, dropAnchor.anchoredPosition, ref posSmoothVelocity, posSmoothSpeed);
-                headTransform.anchoredPosition = currentPos;
+            while ((clonedHead.anchoredPosition - dropAnchor.anchoredPosition).magnitude > posThreshold) {
+                Vector2 currentPos = Vector2.SmoothDamp(clonedHead.anchoredPosition, dropAnchor.anchoredPosition, ref posSmoothVelocity, posSmoothSpeed);
+                clonedHead.anchoredPosition = currentPos;
 
-                Quaternion currentRot = headTransform.rotation;
+                Quaternion currentRot = clonedHead.rotation;
                 Quaternion rotSpeed = Quaternion.Euler(0, 0, rotateSpeed * Time.deltaTime);
-                headTransform.rotation = rotSpeed * currentRot;
+                clonedHead.rotation = rotSpeed * currentRot;
 
                 yield return new WaitForEndOfFrame();
             }
 
-            while (headTransform.anchoredPosition.y > -1200) {
-                Vector2 currentPos = headTransform.anchoredPosition;
+            while (clonedHead.anchoredPosition.y > -1200) {
+                Vector2 currentPos = clonedHead.anchoredPosition;
                 currentPos.y -= dropSpeed * Time.deltaTime;
-                headTransform.anchoredPosition = currentPos;
+                clonedHead.anchoredPosition = currentPos;
 
-                Quaternion currentRot = headTransform.rotation;
+                Quaternion currentRot = clonedHead.rotation;
                 Quaternion rotSpeed = Quaternion.Euler(0, 0, rotateSpeed * Time.deltaTime);
-                headTransform.rotation = rotSpeed * currentRot;
+                clonedHead.rotation = rotSpeed * currentRot;
 
                 yield return new WaitForEndOfFrame();
             }
