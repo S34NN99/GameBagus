ce202ddb933724c4e90793b4689f9631d61c5caf
S34NN99 <seanz995.my@gmail.com>
2022-08-02 19:42:52 +0800

    random stuff

---
+++ leo bod heads app/Assets/SCRIPTS/SaveManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveManager : MonoBehaviour
+{
+
+    public int num;
+    public bool trueorfalse;
+
+    public Manager manager;
+        
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
