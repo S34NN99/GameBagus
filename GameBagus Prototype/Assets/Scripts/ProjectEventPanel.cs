f6d09fbcd313f43b1a7880bdc1e3a4fc7424e746
S34NN99 <seanz995.my@gmail.com>
2022-08-03 16:29:30 +0800

    Update ProjectEventPanel.cs

--- GameBagus Prototype/Assets/Scripts/ProjectEventPanel.cs
+++ GameBagus Prototype/Assets/Scripts/ProjectEventPanel.cs
@@ -1,20 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
-
 using UnityEngine;
 using UnityEngine.Events;
+using TMPro;
 
 public class ProjectEventPanel : MonoBehaviour {
 
-    public void SetContent(string title, string mainBody, string closing) {
+    [SerializeField] private GameObject panelObject;
+    [SerializeField] private TextMeshProUGUI titleText;
+    [SerializeField] private TextMeshProUGUI detailsText;
 
+    public void SetContent(string title, string mainBody, string closing) {
+        titleText.text = title;
+        detailsText.text = mainBody;
     }
 
     public void ShowPanel() {
-
+        panelObject.SetActive(true);
     }
 
     public void HidePanel() {
-
+        panelObject.SetActive(false);
     }
 }
