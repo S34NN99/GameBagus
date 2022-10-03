2a64e30904dde00972a55dd34adcb3de7cb4e2f2
theBlitzzz <65937529+theBlitzz@users.noreply.github.com>
2022-10-04 02:59:08 +0800

    Added head rolling

--- GameBagus Prototype/Assets/Candles/HeadManager.cs
+++ GameBagus Prototype/Assets/Candles/HeadManager.cs
@@ -4,105 +4,39 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class HeadManager : MonoBehaviour {
-    //[SerializeField] private float candleZPos = -10f;
-    [SerializeField] private float decapitationForce = 100f;
+    [SerializeField] private float decapitationForce = 200f;
+    [SerializeField] private float sideForce = 1f;
     [SerializeField] private GameObject colliderTemplate;
-
-
-    [SerializeField] private Transform dropAnchor;
 
     [SerializeField] private IntProperty headCountProp;
 
-    public void RollHead(GameObject head) {
+    public void RollHead(GameObject candleBody) {
         GameObject attachedCollider = Instantiate(colliderTemplate, transform);
-        GameObject clonedHead = Instantiate(head, attachedCollider.transform);
-        clonedHead.transform.localPosition = Vector3.zero;
+        GameObject clonedCandleBody = Instantiate(candleBody);
+
+        attachedCollider.transform.position = candleBody.transform.position;
+        clonedCandleBody.transform.SetParent(attachedCollider.transform);
+        clonedCandleBody.transform.localPosition = Vector3.zero;
+
         Rigidbody2D rb2D = attachedCollider.GetComponent<Rigidbody2D>();
         BoxCollider2D boxCollider = attachedCollider.GetComponent<BoxCollider2D>();
 
-        rb2D.AddForceAtPosition(new Vector2(decapitationForce, 0), clonedHead.transform.position + new Vector3(0, boxCollider.offset.y + boxCollider.size.y / 2));
+        rb2D.AddForceAtPosition(new Vector2(decapitationForce, 0), new Vector3(0, boxCollider.offset.y + boxCollider.size.y / 2));
 
+        StartCoroutine(distributeForce());
         headCountProp.Value++;
-        //StartCoroutine(RollHeadCoroutine());
-
-        //IEnumerator RollHeadCoroutine() {
-        //    clonedHead.anchorMin = new Vector2(0.5f, 0.5f);
-        //    clonedHead.anchorMax = new Vector2(0.5f, 0.5f);
-        //    clonedHead.pivot = new Vector2(0.5f, 0.5f);
-
-        //    Vector2 posSmoothVelocity = new Vector2(0, 0);
-
-        //    while ((clonedHead.anchoredPosition - dropAnchor.anchoredPosition).magnitude > posThreshold) {
-        //        Vector2 currentPos = Vector2.SmoothDamp(clonedHead.anchoredPosition, dropAnchor.anchoredPosition, ref posSmoothVelocity, posSmoothSpeed);
-        //        clonedHead.anchoredPosition = currentPos;
-
-        //        Quaternion currentRot = clonedHead.rotation;
-        //        Quaternion rotSpeed = Quaternion.Euler(0, 0, rotateSpeed * Time.deltaTime);
-        //        clonedHead.rotation = rotSpeed * currentRot;
-
-        //        yield return new WaitForEndOfFrame();
-        //    }
-
-        //    while (clonedHead.anchoredPosition.y > -1200) {
-        //        Vector2 currentPos = clonedHead.anchoredPosition;
-        //        currentPos.y -= dropSpeed * Time.deltaTime;
-        //        clonedHead.anchoredPosition = currentPos;
-
-        //        Quaternion currentRot = clonedHead.rotation;
-        //        Quaternion rotSpeed = Quaternion.Euler(0, 0, rotateSpeed * Time.deltaTime);
-        //        clonedHead.rotation = rotSpeed * currentRot;
-
-        //        yield return new WaitForEndOfFrame();
-        //    }
 
 
-        //}
+        IEnumerator distributeForce() {
+            float timer = 0;
+            while (true) {
+                timer += Time.deltaTime;
+                if (timer >= 2f) {
+                    break;
+                }
+                yield return new WaitForEndOfFrame();
+                rb2D.AddForce(new Vector2(sideForce, 0));
+            }
+        }
     }
-    //[SerializeField] private float posThreshold = 5f;
-    //[SerializeField] private float posSmoothSpeed = 1f;
-    //[SerializeField] private float dropSpeed = 300f;
-    //[SerializeField] private float rotateSpeed = -150f;
-    //[SerializeField] private RectTransform dropAnchor;
-
-    //[SerializeField] private IntProperty headCountProp;
-
-    //public void RollHead(GameObject head) {
-    //    RectTransform clonedHead = Instantiate(head, transform).GetComponent<RectTransform>();
-
-    //    headCountProp.Value++;
-    //    StartCoroutine(RollHeadCoroutine());
-
-    //    IEnumerator RollHeadCoroutine() {
-    //        clonedHead.anchorMin = new Vector2(0.5f, 0.5f);
-    //        clonedHead.anchorMax = new Vector2(0.5f, 0.5f);
-    //        clonedHead.pivot = new Vector2(0.5f, 0.5f);
-
-    //        Vector2 posSmoothVelocity = new Vector2(0, 0);
-
-    //        while ((clonedHead.anchoredPosition - dropAnchor.anchoredPosition).magnitude > posThreshold) {
-    //            Vector2 currentPos = Vector2.SmoothDamp(clonedHead.anchoredPosition, dropAnchor.anchoredPosition, ref posSmoothVelocity, posSmoothSpeed);
-    //            clonedHead.anchoredPosition = currentPos;
-
-    //            Quaternion currentRot = clonedHead.rotation;
-    //            Quaternion rotSpeed = Quaternion.Euler(0, 0, rotateSpeed * Time.deltaTime);
-    //            clonedHead.rotation = rotSpeed * currentRot;
-
-    //            yield return new WaitForEndOfFrame();
-    //        }
-
-    //        while (clonedHead.anchoredPosition.y > -1200) {
-    //            Vector2 currentPos = clonedHead.anchoredPosition;
-    //            currentPos.y -= dropSpeed * Time.deltaTime;
-    //            clonedHead.anchoredPosition = currentPos;
-
-    //            Quaternion currentRot = clonedHead.rotation;
-    //            Quaternion rotSpeed = Quaternion.Euler(0, 0, rotateSpeed * Time.deltaTime);
-    //            clonedHead.rotation = rotSpeed * currentRot;
-
-    //            yield return new WaitForEndOfFrame();
-    //        }
-
-
-    //    }
-    //}
 }
